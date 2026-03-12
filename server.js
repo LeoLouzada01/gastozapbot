f1de36b4198ebcab261049535774d873ce2c81f9
Leonardo <louzadaleo01@gmail.com>
2026-03-11 21:37:20 -0300

    fix server railway

--- server.js
+++ server.js
@@ -1,66 +1,50 @@
 const express = require("express")
 const sqlite3 = require("sqlite3").verbose()
-const path = require("path")
 
 const app = express()
 
 app.use(express.json())
-app.use(express.static("public"))
 
 const db = new sqlite3.Database("./gastos.db")
 
-db.serialize(()=>{
-
 db.run(`
 CREATE TABLE IF NOT EXISTS gastos(
- id INTEGER PRIMARY KEY AUTOINCREMENT,
- descricao TEXT,
- valor REAL,
- data TEXT
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+descricao TEXT,
+valor REAL
 )
 `)
 
+app.get("/", (req,res)=>{
+res.send("GastoZap rodando 🚀")
 })
 
-app.get("/",(req,res)=>{
- res.send("GastoZap Online 🚀")
-})
-
-app.get("/api/gastos",(req,res)=>{
+app.get("/gastos",(req,res)=>{
 
 db.all("SELECT * FROM gastos",(err,rows)=>{
+if(err) return res.send(err)
 
- if(err){
-  res.json({erro:err})
- }else{
-  res.json(rows)
- }
+res.json(rows)
 
 })
 
 })
 
-app.post("/api/gastos",(req,res)=>{
+app.post("/gastos",(req,res)=>{
 
-const {descricao,valor} = req.body
+const {descricao,valor}=req.body
 
 db.run(
-"INSERT INTO gastos(descricao,valor,data) VALUES(?,?,datetime('now'))",
-[descricao,valor],
-function(err){
+"INSERT INTO gastos(descricao,valor) VALUES(?,?)",
+[descricao,valor]
+)
 
- if(err){
-  res.json({erro:err})
- }else{
-  res.json({ok:true})
- }
-
-})
+res.send("ok")
 
 })
 
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT,"0.0.0.0",()=>{
- console.log("Servidor rodando na porta "+PORT)
+console.log("Servidor rodando porta "+PORT)
 })
